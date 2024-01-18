78341b252c6ac240a02d837425dc20915f3d4148
danielntow <danielntow@gmail.com>
2024-01-18 01:04:53 +0000

    update place

--- models/place.py
+++ models/place.py
@@ -19,4 +19,4 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    # amenity_ids = []
