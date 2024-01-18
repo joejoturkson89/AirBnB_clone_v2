8dd8b522fab759264cf388964a957b41e22c4e2a
danielntow <danielntow@gmail.com>
2024-01-18 05:53:30 +0000

    update amenities

--- models/place.py
+++ models/place.py
@@ -16,7 +16,9 @@ place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'), primary_key=True, nullable=False))
+                             ForeignKey(
+                                 'amenities.id'
+                      ), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
