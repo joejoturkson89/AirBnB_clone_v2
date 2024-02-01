2ab1e1b2d86be3b5be791b5acb79450719fcbada
joejoturkson89 <turksonjoejo@gmail.com>
2024-02-01 09:16:35 -0800

    set up web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This script sets up web servers for the development of web static
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
@@ -10,7 +10,7 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/releases/test/index.html
-sudo echo "<html>
+sudo echo -e "<html>
   <head>
   </head>
   <body>
@@ -20,5 +20,7 @@ sudo echo "<html>
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+file=/etc/nginx/sites-available/default
+phrase="location /hbnb_static/{\n\talias /data/web_static/current/;\n}\n"
+sudo sed -i "27i $phrase" $file
 sudo service nginx restart
