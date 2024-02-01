2294adfd82c37a7d99623090652c62749b7b3dda
joejoturkson89 <turksonjoejo@gmail.com>
2024-02-01 09:30:41 -0800

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,26 +3,17 @@
 import os
 from datetime import datetime
 from fabric.api import local, runs_once
+import tarfile
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    date_time = datemine.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        date_time.year,
-        date_time.month,
-        date_time.day,
-        date_time.hour,
-        date_time.minute,
-        date_time.second
-    )
+    
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
-    except Exception:
-        output = None
-    return output
+        name = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S")
+        local('mkdir -p versions')
+        local("tar -cvzf versions/{}.tgz {}".format(
+            name, "web_static/"))
+        size = os.path.getsize("./version/{}.tgz".format(name))
+        print("web_static packed: versions/{}.tgz -> {}Bytes".format(
+            name, size))
+    except:
+        return None
