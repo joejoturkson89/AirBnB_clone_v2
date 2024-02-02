9ee8339f7d58e4f923a4a2abace5f6f0b5fe1eff
joejoturkson89 <turksonjoejo@gmail.com>
2024-02-02 04:42:38 -0800

    1 web static pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,8 +2,9 @@
 """This module for Fabric script that generates a tgz archive."""
 import os
 from datetime import datetime
-from fabric.api import local, runs_once
+from fabric.api import local
 import tarfile
+
 
 def do_pack():
     
@@ -12,7 +13,7 @@ def do_pack():
         local('mkdir -p versions')
         local("tar -cvzf versions/{}.tgz {}".format(
             name, "web_static/"))
-        size = os.path.getsize("./version/{}.tgz".format(name))
+        size = os.path.getsize("./versions/{}.tgz".format(name))
         print("web_static packed: versions/{}.tgz -> {}Bytes".format(
             name, size))
     except:
